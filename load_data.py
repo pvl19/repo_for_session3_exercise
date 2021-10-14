6e058040dad8fbfca7526aa7b520bb293f9160cb
Mubdi Rahman <mubdi@sidratresearch.com>
2021-10-14 11:47:44 -0400

    Editing base file

--- load_data.py
+++ load_data.py
@@ -1,4 +1,4 @@
 import numpy as np
 import pandas as pd
 
-# In this file, you'll put all the functions for loading data.
+# In this file, you'll put all the functions for creating the Fibonacci sequence up to a certain number.
