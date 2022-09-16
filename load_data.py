4ddd44b0d56c3157ef389aaa304872ac81b3f416
Braden Gail <bradengail@Bradens-MacBook-Pro-2.local>
2022-09-16 16:53:51 -0400

    adding my version of the fib sequence

--- load_data.py
+++ load_data.py
@@ -1,4 +1,19 @@
 import numpy as np
-import pandas as pd
 
 # In this file, you'll put all the functions for creating the Fibonacci sequence up to a certain number.
+
+
+def fibSequence(n):
+    sequence = []
+    if n<=0:
+        return print("Please specify a whole number!")
+    else:
+        sequence.append(0)
+        if n==2:
+            sequence.append(1)
+        else:
+            sequence.append(1)
+            for i in range(n-2):
+                sequence.append(sequence[-1]+sequence[-2])
+    return sequence
+
