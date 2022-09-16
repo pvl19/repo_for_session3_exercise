e860871c99c5e414ca0f70e6ea39c528d9cb5e12
Phil Van-Lane <philvanlane@Phils-MacBook-Pro.local>
2022-09-16 16:51:27 -0400

    fiboroni rigatoni

--- load_data.py
+++ load_data.py
@@ -2,3 +2,15 @@ import numpy as np
 import pandas as pd
 
 # In this file, you'll put all the functions for creating the Fibonacci sequence up to a certain number.
+def fiboroni(num):
+    fib = [1,1]
+    if num < 1:
+        print("Nope.")
+    elif num == 1:
+        print([1])
+    elif num == 2:
+        print([1,1])
+    else:
+        for i in range(2, num):
+            fib.append(fib[i-1] + fib[i - 2])
+        print(fib)
